fb32edd29a4822db8b244ebe62d63dc475900c47
jasperobiacoro <109198362+jasperobiacoro@users.noreply.github.com>
2023-09-19 22:29:26 -0700

    Update main.py
    Return something simple in the webpage

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+from fastapi.responses import HTMLResponse
+
+@app.get("/", response_class=HTMLResponse)
+def homepage():
+    return """
+    <html>
+    <head>
+        <title>CodeCraft Collective</title>
+    </head>
+    <body>
+        <h1>Welcome!</h1>
+        <p>Use this website to make learning fun!</p>
+    </body>
+    </html>
+    """
