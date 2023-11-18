384d71184eca0e29dd8e04d66cbd4b938e46380d
Ongor3 <cabrerajulian3654@gmail.com>
2023-11-18 15:04:54 -0800

    Removed pycache

--- website/main/migrations/0001_initial.py
+++ website/main/migrations/0001_initial.py
@@ -15,17 +15,23 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ToDoList',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200)),
             ],
         ),
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID')),
                 ('text', models.CharField(max_length=300)),
                 ('complete', models.BooleanField()),
-                ('todolist', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='main.todolist')),
+                ('todolist', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='main.todolist')),
             ],
         ),
     ]
