f171adbaf148d491ac2417eb8088da9029293cb8
abdiel7382 <104485825+abdiel7382@users.noreply.github.com>
2023-12-04 21:58:42 -0800

    Update views.py

--- website/registration/views.py
+++ website/registration/views.py
@@ -2,7 +2,25 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from .forms import RegisterForm
+from django.urls import path
+from .views import MyLoginView, RegisterView
 
+from django.contrib.auth.views import (
+    LogoutView, 
+    PasswordResetView, 
+    PasswordResetDoneView, 
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
+urlpatterns = [
+    path('login/', MyLoginView.as_view(redirect_authenticated_user=True),name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'),name='logout'),
+    path('register/', RegisterView.as_view(),name='register'),
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html'),name='password-reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
+]
 
 def registration(response):
     if response.method == "POST":
