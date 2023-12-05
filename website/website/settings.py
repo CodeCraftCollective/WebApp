faf8f70030ab6930ff22764710ff1fdbcceed24e
abdiel7382 <104485825+abdiel7382@users.noreply.github.com>
2023-12-05 09:01:48 -0800

    Update settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -128,11 +128,11 @@ LOGOUT_REDIRECT_URL = "/"
 LOGIN_REDIRECT_URL = "/"
 # email configs
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = str(os.getenv('abdiel7382@gmail.com'))
-EMAIL_HOST_PASSWORD = str(os.getenv('usni hchx ymjs fzde'))
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
