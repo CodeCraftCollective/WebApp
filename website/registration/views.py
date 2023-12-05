ed18c03b4395e85d4b547470c1b3a0d783770104
abdiel7382 <104485825+abdiel7382@users.noreply.github.com>
2023-12-05 01:58:22 -0800

    Update views.py

--- website/registration/views.py
+++ website/registration/views.py
@@ -2,19 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from .forms import RegisterForm
-from django.urls import path
+from django.urls import path, reverse_lazy
 #from .views import MyLoginView, RegisterView
 
 from django.contrib.auth.views import PasswordResetView
 from django.contrib.messages.views import SuccessMessageMixin
 
 class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
     email_template_name = 'registration/password_reset_email.html'
     subject_template_name = 'registration/password_reset_subject'
     success_message = "We've emailed you instructions for setting your password, " \
                       "if an account exists with the email you entered. You should receive them shortly." \
                       " If you don't receive an email, " \
                       "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('users-home')
 
 def registration(response):
     if response.method == "POST":
