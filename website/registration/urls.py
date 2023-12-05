a9723cdd93436943c606e151e5fd0ddcfc03d438
abdiel7382 <104485825+abdiel7382@users.noreply.github.com>
2023-12-05 02:36:22 -0800

    Update urls.py

--- website/registration/urls.py
+++ website/registration/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from users.views import ResetPasswordView
 from . import views
 
 
@@ -7,6 +6,4 @@ from . import views
 urlpatterns = [
     path("registration/", views.registration, name="registration"),
     path("login/", views.login_user, name="login"),
-    path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
-    
 ]
