dd64dea790d1629ba271008e3be97c45bcd14523
Ongor3 <cabrerajulian3654@gmail.com>
2023-12-09 05:35:19 -0800

    Created menu for hangman

--- website/hangman/views.py
+++ website/hangman/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-def hangman(response):
+from main.models import ToDoList
+
+def hangman(response, id):
+    ls = ToDoList.objects.get(id=id)
+    if ls in ToDoList.objects.all():
+        items = ls.item_set.all()
+        words = [item.text for item in items]
+        return render(response, 'hangman/hangman.html', {'words': words})
     return render(response, "hangman/hangman.html", {})
+
+def menu(response):
+    ls = ToDoList.objects.filter(user=response.user)
+    return render(response, "hangman/menu.html", {"ls": ls})
